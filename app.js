2abe1c9228bd3944b8da48a786ce804a10f99deb
Muhammad Saad <itsmsaadhere@gmail.com>
2021-02-16 17:14:59 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+(function () {
+    const form = document.querySelector('#search-form');
+    const searchField = document.querySelector('#search-keyword');
+    let searchedForText;
+    const responseContainer = document.querySelector('#response-container');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        responseContainer.innerHTML = '';
+        searchedForText = searchField.value;
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+    headers: {
+        Authorization: 'Client-ID P8ySS33GeEYwezJGVvTZDlfH5ePcvNgqF1R374sAu1A'
+    }
+}).then(response => response.json())
+.then(addImage)
+.catch(e => requestError(e, 'image'));
+fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=ZrUKTCwLkbzEfhqzH4awjS8o8k7PvIGs`)
+.then(response=> response.json()).then(addArticles).catch(e=>requestError(e,'articles'))
+    
+    });
+    function addImage(data) {
+        let htmlContent = '';
+        const firstImage = data.results;
+       
+    
+        if (firstImage) {
+        
+            htmlContent = '<ul>'+ firstImage.map(firstImage=> `<figure>
+                <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`).join('') + '<ul>';
+            
+        } 
+       
+        else {
+            htmlContent = `<div class= "error-no-image" >No Images Available </div>`
+        }
+    
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+    function addArticles(data){
+        console.log(data)
+        if(data.response && data.response.docs){
+          const article=data.response.docs
+            htmlContent= '<ul>' +  article.map(article=> `<li class="article"  >
+            <h2> <a href="${article.web_url}"> ${article.headline.main} </a> </h2>
+            <p>${article.snippet} </p>
+            </li>`).join('') + '</ul>'
+        } else{
+            htmlContent= `<div class= "error-no-articles" >No Article Available </div>`
+  
+        }
+        responseContainer.insertAdjacentHTML("beforeend",htmlContent)
+            
+          
+        
+  
+    }
+    
+    
+
+    function requestError(e, part) {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
+    }
+})();
